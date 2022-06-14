5d5e81ee69c79951bf7be4f428b1ff1fd526bcc8
RaylanderO <raylandersantos313@gmail.com>
2022-06-13 21:35:18 -0400

    os que eu não mandei no dia certo

---
+++ soma.kt
@@ -0,0 +1,13 @@
+fun soma(x:Int, y:Int):Int{
+    return x+y
+
+}
+fun sub(x:Int, y:Int):Int{
+    return x-y
+}
+fun main(){
+    println("Soma de dois números")
+    println(soma(4,3))
+    println("Subtração de dois números")
+    println(sub(4,3))
+}
---
+++ sub.kt
@@ -0,0 +1,7 @@
+fun sub(x:Int, y:Int):Int{
+    return x-y
+}
+fun main(){
+    println("Subtração de dois números")
+    println(sub(4,3))
+}
